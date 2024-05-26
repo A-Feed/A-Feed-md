fa00390e3ec01ee71080e9a6784da72fa3a6c666
Qushai121 <m.qushai111@gmail.com>
2024-05-26 14:35:48 +0700

    adding some resource theme and colors, litle reusable components

--- app/src/main/java/com/capstone/afeed/ui/MonitoringActivity.kt
+++ app/src/main/java/com/capstone/afeed/ui/MonitoringActivity.kt
@@ -1,23 +1,33 @@
-package com.capstone.afeed
+package com.capstone.afeed.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
+import com.capstone.afeed.R
+import com.capstone.afeed.databinding.ActivityMonitoringBinding
 
-class MainActivity : AppCompatActivity() {
+class MonitoringActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMonitoringBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMonitoringBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setSupportActionBar(
+//            binding.includedToolbarMain.tollbarMain
+//        )
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+
 }
--- app/src/main/java/com/capstone/afeed/ui/MonitoringDetailActivity.kt
+++ app/src/main/java/com/capstone/afeed/ui/MonitoringDetailActivity.kt
@@ -1,19 +1,17 @@
-package com.capstone.afeed
+package com.capstone.afeed.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
+import com.capstone.afeed.R
 
-class MainActivity : AppCompatActivity() {
-
+class MonitoringDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_monitoring_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
